f2db09b10c75214f6627e8d9f62f8ba2071b333c
Noto <1358519169@qq.com>
2020-11-13 14:24:32 +0800

    使用PositionType重写shape类

--- lib/shape/shape.lua
+++ lib/shape/shape.lua
@@ -1,26 +1,26 @@
 ---@class Shape
 local Shape = Object:extend()
 
+---@param x number
+---@param y number
+---@param z number
+---@param cFill ColorType
+---@param cLine ColorType
+---@param cMesh ColorType
 function Shape:new(x, y, z, cFill, cLine, cMesh)
-    self.x = x
-    self.y = y
-    self.z = z
+    self.position = Base.createPosition(x, y, z)
 
-    -- fill
-    self.cFill = Base.color.fill
-	if cFill ~= nil then
-		self.cFill = cFill
-    end
-    -- line
-	self.cLine = Base.color.line
-	if cLine ~= nil then
-		self.cLine = cLine
-    end
-    -- mesh
-    self.cMesh = Base.color.white
-	if cMesh ~= nil then
-        self.cMesh = cMesh
-    end
+    self.cFill = Base.ternary(cFill ~= nil,     cFill,      Base.color.fill)
+    self.cLine = Base.ternary(cLine ~= nil,     cLine,      Base.color.line)
+    self.cMesh = Base.ternary(cMesh ~= nil,     cMesh,      Base.color.white)
+end
+
+---@param position PositionType
+---@param cFill ColorType
+---@param cLine ColorType
+---@param cMesh ColorType
+function Shape:new(position, cFill, cLine, cMesh)
+    Shape:new(position.x, position.y, position.z, cFill, cLine, cMesh)
 end
 
 return Shape
