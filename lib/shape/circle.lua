89185fd8ca55bd2dd66d23bcf403f76173b305a3
Noto <1358519169@qq.com>
2020-11-14 21:11:07 +0800

    规范colorXXX变量

--- lib/shape/circle.lua
+++ lib/shape/circle.lua
@@ -1,7 +1,7 @@
 local Circle = Shape:extend()
 
-function Circle:new(x, y, z, radius, cFill, cLine, cMesh)
-	Circle.super.new(self, x, y, z, cFill, cLine, cMesh)
+function Circle:new(x, y, z, radius, colorFill, colorLine, colorMesh)
+	Circle.super.new(self, x, y, z, colorFill, colorLine, colorMesh)
 
 	self.radius = radius
 end
@@ -9,13 +9,13 @@ end
 function Circle:draw(mode)
 
 	if mode == 0 then
-		love.graphics.setColor(self.cFill)
+		love.graphics.setColor(self.colorFill)
 		love.graphics.circle('fill' , self.position.x, self.position.y, self.radius)
 
-		love.graphics.setColor(self.cLine)
+		love.graphics.setColor(self.colorLine)
 		love.graphics.circle('line' , self.position.x, self.position.y, self.radius)
 	elseif mode == 1 then
-		love.graphics.setColor(self.cLine)
+		love.graphics.setColor(self.colorLine)
 		love.graphics.line(self.position.x - self.radius, self.position.z, self.position.x + self.radius, self.position.z)
 	else
 		local x1 = self.position.x
@@ -23,10 +23,10 @@ function Circle:draw(mode)
 		local x2 = self.radius
 		local y2 = self.radius * (1 - mode)
 
-		love.graphics.setColor(self.cFill)
+		love.graphics.setColor(self.colorFill)
 		love.graphics.ellipse('fill', x1, y1, x2, y2)
 
-		love.graphics.setColor(self.cLine)
+		love.graphics.setColor(self.colorLine)
 		love.graphics.ellipse('line', x1, y1, x2, y2)
 	end
 end
